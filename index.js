be6e429ac12a50b0ccf48cb37d937624b32784f8
Stefano Longhi <stefano@moneyfarm.com>
2020-04-28 10:14:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const uuid = require("uuid/v4");
+
+exports.handler = async (event) => {
+  let quantity =1;
+  if(!(event.quantity*1).isNaN){
+    quantity = event.quantity;  
+  }
+  quantity =Math.min(quantity,20);
+  let uuidsGenerated= [];
+  for (var i = 0; i < quantity; i++) {
+    uuidsGenerated.push(uuid());
+  }
+  
+  let response = {
+      statusCode: 200,
+      headers: {
+            "Content-Type": "application/json"
+        },
+      body: {
+          uuids: uuidsGenerated
+        }
+      //JSON.stringify(uuidsGenerated)
+  };
+
+  return response;
+};
